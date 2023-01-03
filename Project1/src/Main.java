4d0dce76a0aff086c66405b2dc7fcfed69517b34
Fei Lin <90736406+walletkun@users.noreply.github.com>
2023-01-03 07:22:10 -0800

    Add files via upload

---
+++ Project1/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		for(int i = 1; i <= 100; i++) {
+				System.out.print(i + "  " );
+				if(i % 5 == 0) {
+					System.out.print("\n");
+				}
+		}
+
+	}
+}
